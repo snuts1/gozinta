02bc46f3960ac64ea16a96dd8ff6213460d912de
steev <nuny@biznis>
2025-06-14 11:17:55 -0400

    fix icon path3

--- vite.config.js
+++ vite.config.js
@@ -25,16 +25,16 @@ export default defineConfig({
         start_url: '/',
         icons: [
           {
-            src: 'gg-192x192.png', // You'd place this in your public folder
+            src: 'icons/gg-192x192.png', // You'd place this in your public folder
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: 'gg-512x512.png', // You'd place this in your public folder
+            src: 'icons/gg-512x512.png', // You'd place this in your public folder
             sizes: '512x512',
             type: 'image/png',
           },{
-            src: 'gg-maskable-512x512.png', // You'd place this in your public folder
+            src: 'icons/gg-maskable-512x512.png', // You'd place this in your public folder
             sizes: '512x512',
             type: 'image/png',
             purpose: 'maskable'
